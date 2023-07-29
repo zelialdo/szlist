55e0b4347dc777d81f5a9a122a58cecc7f53f1a5
Zelialdo Sueldo <71799490+zelialdo@users.noreply.github.com>
2023-07-28 21:47:42 -0300

    Project created

---
+++ src/main/java/com/devsz/szlist/SzlistApplication.java
@@ -0,0 +1,13 @@
+package com.devsz.szlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsz/szlist/SzlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsz.szlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
